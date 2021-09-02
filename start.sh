a693d03910a1d667522d1ff8bd49c497fe2d8ba7
Yolok0306 <jkluio785636@gmail.com>
2021-09-03 01:14:49 +0800

    adjust start.sh and stop.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
-PID=0
-if [ -f './pid' ]; then
-    PID=$(cat ./pid)
-fi
 
-if ps -p $PID > /dev/null
-then
-   echo "he1pME-1.1.jar with $PID is already running"
-else
-    nohup java -jar target/he1pME-1.1.jar > he1pME.log 2> error.log < /dev/null & PID=$!; echo $PID > ./pid
-fi
+nohup java -jar target/he1pME-1.1.jar & 
