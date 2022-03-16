354bf4dbd8e4af78bd98d301c431eccb9596e222
Yolok0306 <jkluio785636@gmail.com>
2022-03-16 22:56:59 +0800

    adjust regex and add addRole function

--- src/main/java/Action/EditNameAction.java
+++ src/main/java/Action/EditNameAction.java
@@ -2,10 +2,7 @@ package Action;
 
 import Util.CommonUtil;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Member;
 import discord4j.core.spec.GuildMemberEditSpec;
-
-import java.util.Objects;
 
 public class EditNameAction implements Action {
     @Override
@@ -15,18 +12,18 @@ public class EditNameAction implements Action {
 
     @Override
     public void execute(final MessageCreateEvent event) {
-        final String content = event.getMessage().getContent().toString();
-
-        event.getMessage().getMemberMentions().stream().findFirst().ifPresent(partialMember -> {
-            final Member member = partialMember.asFullMember().block();
-            final String oldName = Objects.requireNonNull(member).getDisplayName();
-            final String newName = content.replaceAll("\\$editName\\s<@[!]\\d{18}>\\s++", "");
-            final GuildMemberEditSpec guildMemberEditSpec = GuildMemberEditSpec.builder().build();
-            Objects.requireNonNull(member).edit(guildMemberEditSpec.withNicknameOrNull(newName)).block();
-            final String title = "修改暱稱成功";
-            final String desc = oldName + " -> " + newName;
-            final String thumb = Objects.requireNonNull(member).getAvatarUrl();
-            CommonUtil.replyByHe1pMETemplate(event, title, desc, thumb);
-        });
+        event.getMessage().getMemberMentions().stream().findFirst().ifPresent(partialMember ->
+                partialMember.asFullMember().subscribe(member -> {
+                    final String oldName = member.getDisplayName();
+                    final String regex = "\\" + CommonUtil.SIGN + "editName\\p{Blank}<@!\\d{18}>\\p{Blank}++";
+                    final String newName = event.getMessage().getContent().replaceAll(regex, "");
+                    final GuildMemberEditSpec guildMemberEditSpec = GuildMemberEditSpec.builder().build();
+                    member.edit(guildMemberEditSpec.withNicknameOrNull(newName)).block();
+                    final String title = "修改暱稱成功";
+                    final String desc = oldName + " -> " + newName;
+                    final String thumb = member.getAvatarUrl();
+                    CommonUtil.replyByHe1pMETemplate(event, title, desc, thumb);
+                })
+        );
     }
 }
